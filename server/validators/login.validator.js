cb5eff0db6b06c1cf3c53549fb56a5faaa965245
justincuzin <justincuzin@gmail.com>
2023-02-28 09:52:20 +0100

    Clean wrong name

--- server/validators/login.validator.js
+++ server/validators/login.validator.js
@@ -1,8 +1,8 @@
 const Joi = require('joi');
 
-const registerSchema = Joi.object({
+const loginSchema = Joi.object({
 	email: Joi.string().email().required(),
 	password: Joi.string().min(6).required(),
 });
 
-module.exports = registerSchema;
+module.exports = loginSchema;
