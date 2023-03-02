1a82d1e851002035b8d8c0ec12e6ae0698daf58e
justincuzin <justincuzin@gmail.com>
2023-03-02 21:20:43 +0100

    Add handling errors for signup route and during fetching DB

--- server/middlewares/validator.js
+++ server/middlewares/validator.js
@@ -1,22 +1,17 @@
-const createHttpError = require('http-errors');
-
 const validators = require('../validators');
 
-module.exports = function(validator) {
-
+module.exports = function (validator) {
 	// eslint-disable-next-line no-prototype-builtins
-	if(!validators.hasOwnProperty(validator))
+	if (!validators.hasOwnProperty(validator))
 		throw new Error(`'${validator}' validator is not exist`);
 
-	return async function(req, res, next) {
+	return async function (req, res, next) {
 		try {
 			const validated = await validators[validator].validateAsync(req.body);
 			req.body = validated;
 			next();
 		} catch (err) {
-			if(err.isJoi) 
-				return next(createHttpError(422, {message: err.message}));
-			next(createHttpError(500));
+			return res.json({ success: false, error: err.message });
 		}
 	};
 };
