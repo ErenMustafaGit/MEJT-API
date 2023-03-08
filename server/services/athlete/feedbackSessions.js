c34505d6466117ae4522e32626ca81aec77ec9c4
TheoHauray <theohauray44@gmail.com>
2023-03-08 20:01:59 +0100

    fix: return value of create feedback route

--- server/services/athlete/feedbackSessions.js
+++ server/services/athlete/feedbackSessions.js
@@ -5,7 +5,6 @@ const {
 
 const createAthleteFeedback = async (userId, sessionId, shape, tiredness, stress, sensation, injury) => {
     const feedbackCreated = await createFeedbackSession(userId, sessionId, shape, tiredness, stress, sensation, injury);
-    console.log('feedbackCreated :>> ', feedbackCreated);
 
 	return feedbackCreated;
 };
