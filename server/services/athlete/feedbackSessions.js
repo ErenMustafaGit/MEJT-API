68c96d4b61d6deb346497a242082cfcf868b1acb
justincuzin <justincuzin@gmail.com>
2023-03-09 07:18:35 +0100

    Fix issue with GET /athlete/feedbackSession

--- server/services/athlete/feedbackSessions.js
+++ server/services/athlete/feedbackSessions.js
@@ -6,13 +6,13 @@ const {
 
 
 const createAthleteFeedback = async (userId, sessionId, shape, tiredness, stress, sensation, injury) => {
-    const feedbackCreated = await createFeedbackSession(userId, sessionId, shape, tiredness, stress, sensation, injury);
+	const feedbackCreated = await createFeedbackSession(userId, sessionId, shape, tiredness, stress, sensation, injury);
 
 	return feedbackCreated;
 };
 
 const getFeedbackSessionIfProvided = async (userId, sessionId) => {
-    const feedbackProvided = await getFeedbackSessionByUserId(userId, sessionId);
+	const feedbackProvided = await getFeedbackSessionByUserId(userId, sessionId);
 
 	if(isEmpty(feedbackProvided))
 	{
@@ -23,7 +23,7 @@ const getFeedbackSessionIfProvided = async (userId, sessionId) => {
 		const formattedFeedbackProvided = { 
 			sessionId: feedbackProvided[0].sessionId, 
 			name: feedbackProvided[0].sessions.name, 
-			shape: feedbackProvided[0].feedbackProvided,
+			shape: feedbackProvided[0].shape,
 			tiredness: feedbackProvided[0].tiredness,
 			stress: feedbackProvided[0].stress,
 			sensation: feedbackProvided[0].sensation,
