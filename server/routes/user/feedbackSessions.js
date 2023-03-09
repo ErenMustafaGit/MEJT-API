2e8f120b5b62727bb31d8790748d2db2365314a0
TheoHauray <theohauray44@gmail.com>
2023-03-09 15:22:33 +0100

    change athlete/feedbackSession to user/feedbackSession

--- server/routes/user/feedbackSessions.js
+++ server/routes/user/feedbackSessions.js
@@ -1,10 +1,13 @@
 const express = require('express');
-const { isEmpty, toInteger } = require('lodash');
+const { isEmpty } = require('lodash');
 const router = express.Router();
 const passport = require('passport');
 const { parse } = require('path');
 const { getUserById } = require('../../repositories/user.repository');
-const { getFeedbacksByAthleteId } = require('../../services/user/feedbackSessions');
+const { 
+    getFeedbacksByAthleteId,
+    getFeedbackSessionIfProvided,
+} = require('../../services/user/feedbackSessions');
 
 require('../../middlewares/auth');
 
@@ -34,6 +37,64 @@ router.get('/user/feedbackSessions', async (req, res, next) => {
 			const athlete = await getFeedbacksByAthleteId(athleteId, teamId, startingDate, endingDate);
 			const formattedFeedbacks = { success: true, athlete };
 			return res.send(formattedFeedbacks);
+		}
+	})(req, res, next);
+});
+
+router.get('/user/singleFeedbackSession', async (req, res, next) => {
+	passport.authenticate('jwt', { session: false }, async (err, user) => {
+		if (!user) 
+        {
+			return res.json({
+				success: false,
+				error: 'The token is empty or is invalid',
+			});
+		}
+        else 
+        {
+			const userLogged = await getUserById(user.id);
+            let athelteId = user.id;
+
+            if(userLogged.type != 1)
+            {
+                if(req.query.athleteId != null)
+                {
+                    athelteId = parseInt(req.query.athleteId);
+                }
+                else
+                {
+                    return res.json({
+                        success: false,
+                        error: 'Athelte ID is missing',
+                    });
+                }
+            }
+
+            let sessionId;
+
+            if(req.query.sessionId == null)
+            {
+                return res.json({
+                    success: false,
+                    error: 'Session ID is missing',
+                });
+            }
+            else
+            {
+                sessionId = parseInt(req.query.sessionId);
+            }
+
+            let feedbackProvided;
+            try {
+                feedbackProvided = await getFeedbackSessionIfProvided(athelteId, sessionId);
+            }
+            catch(err)
+            {
+                return res.send({ success: false , error: {err}})
+            }
+            const formattedFeedbackProvided = { success: true , sessionFeedback: (isEmpty(feedbackProvided) ? null : feedbackProvided)};
+
+            return res.send(formattedFeedbackProvided);
 		}
 	})(req, res, next);
 });
