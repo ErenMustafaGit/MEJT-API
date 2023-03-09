811177c462d656acf88f6d2d40de17501e8908b0
justincuzin <justincuzin@gmail.com>
2023-03-09 20:10:06 +0100

    Fix issue with rebase and remove unused test route

--- server/routes.js
+++ server/routes.js
@@ -1,8 +1,8 @@
 const authRoute = require('./routes/auth');
-const testRoute = require('./routes/test');
 const trainerTeamsRoute = require('./routes/trainer/teams');
 const athleteTeamsRoute = require('./routes/athlete/teams');
 const athleteSessionsRoute = require('./routes/athlete/sessions');
+const trainerSessionsRoute = require('./routes/trainer/sessions');
 const trainerTeamFeedbacksRoute = require('./routes/trainer/feedbackSessions');
 const trainerTeamInformationRoute = require('./routes/trainer/teamInformation');
 const userAthleteFeedbacksRoute = require('./routes/user/feedbackSessions');
@@ -10,10 +10,10 @@ const athleteFeedbacksRoute = require('./routes/athlete/feedbackSessions');
 
 const allRoutes = [
 	authRoute,
-	testRoute,
 	trainerTeamsRoute,
 	athleteTeamsRoute,
 	athleteSessionsRoute,
+	trainerSessionsRoute,
 	trainerTeamFeedbacksRoute,
 	trainerTeamInformationRoute,
 	userAthleteFeedbacksRoute,
